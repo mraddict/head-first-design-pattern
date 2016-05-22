f92649941131661249e7e37f218868bfaa6c6926
mraddict <mraddict44@gmail.com>
2016-05-22 15:56:10 +0900

    싱글톤 예제

---
+++ design-pattern-singleton-threadsafe/src/SingletonClient.java
@@ -0,0 +1,11 @@
+
+public class SingletonClient
+{
+	public static void main( String [] args )
+	{
+		Singleton singleton = Singleton.getInstance();
+		
+		System.out.println( singleton.getDescription() );
+	}
+
+}
